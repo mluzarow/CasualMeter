8b0ba6212f6525d6b826362f73b97352a49f42e5
Gl0 <ab@glk.spb.ru>
2016-07-30 17:44:27 +0300

    Add new dungeons to site export

--- CasualMeter.Common/TeraDpsApi/DataExporter.cs
+++ CasualMeter.Common/TeraDpsApi/DataExporter.cs
@@ -174,7 +174,11 @@ namespace CasualMeter.Common.TeraDpsApi
                 areaId != 767 &&
                 areaId != 768 &&
                 areaId != 470 &&
-                areaId != 468
+                areaId != 468 &&
+                areaId != 770 &&
+                areaId != 769 &&
+                areaId != 969 &&
+                areaId != 970
                 )
             {
                 return;
